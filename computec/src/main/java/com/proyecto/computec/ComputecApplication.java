7d112f19441884003ec9760b112c446d4605cae7
BrunoRub <brunorrubio@gmail.com>
2021-11-05 15:33:37 -0500

    Proeycto

---
+++ computec/src/main/java/com/proyecto/computec/ComputecApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.computec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputecApplication.class, args);
+	}
+
+}
